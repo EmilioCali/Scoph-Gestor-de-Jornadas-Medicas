59b26dcffb3a852247d1ea8707038699ba086036
ecatalan-2024133 <ecatalan-2024133@kinal.edu.gt>
2026-05-14 03:21:43 -0600

    feat(add): Agregar funcionalidad de audit.service.js

---
+++ core-service/src/modules/audit/audit.service.js
@@ -0,0 +1,30 @@
+import Audit from './audit.model.js'
+
+export async function registerAudit({
+    userId,
+    action,
+    module,
+    reference,
+    description
+}) {
+
+    try {
+
+        return await Audit.create({
+            userId,
+            action,
+            module,
+            reference,
+            description
+        })
+
+    } catch (error) {
+
+        console.error(
+            'Error registrando auditoría',
+            error
+        )
+
+    }
+
+}
