21567eff517fdce7da29a26ebe86ebda0cf5000b
ecatalan-2024133 <ecatalan-2024133@kinal.edu.gt>
2026-05-14 03:19:55 -0600

    feat(add): Agregar funcionalidad de audit.messages.js

---
+++ core-service/src/modules/audit/audit.messages.js
@@ -0,0 +1,13 @@
+export const AUDIT_MESSAGES = {
+    CONSUMO_JORNADA:
+        'Consumo de jornada registrado',
+
+    RETORNO_JORNADA:
+        'Retorno de jornada registrado',
+
+    TRANSFERENCIA:
+        'Transferencia registrada',
+
+    ENTRADA:
+        'Entrada registrada'
+}
